2158bfee379631dc4c6f5bea307f3d890b089a72
icyveins7 <icyveins@hotmail.com>
2024-09-25 10:28:20 +0000

    add some tests on pcall

--- lua/test.lua
+++ lua/test.lua
@@ -1,1 +1,31 @@
+for i=3,1,-1 do
+  io.write(string.format("%d\n", i))
+end
+
 print("hello from lua")
+
+function checkStructure(args)
+  if type(args.oWidth) ~= "number" then
+    print("oWidth="..args.oWidth)
+    error("must specify byte offset to width")
+  end
+  print("structure ok")
+
+  return 0
+end
+
+checkStructure{oWidth=1}
+
+local success, returnval = pcall(
+  function()
+    checkStructure{oWidth=1}
+  end
+)
+print(success)
+print(returnval)
+
+-- cannot pcall with table input?
+--local success = pcall(checkStructure{oWidth=1})
+if success then
+  print("test case passed")
+end
